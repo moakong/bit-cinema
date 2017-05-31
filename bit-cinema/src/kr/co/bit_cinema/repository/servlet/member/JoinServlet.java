c0d4e14057b54ea72badad8cf6aa724a4300916e
mingyeong <lovp576@gmail.com>
2017-05-31 20:52:23 +0900

    회원가입 가능

--- bit-cinema/src/kr/co/bit_cinema/repository/servlet/member/JoinServlet.java
+++ bit-cinema/src/kr/co/bit_cinema/repository/servlet/member/JoinServlet.java
@@ -13,6 +13,7 @@ import org.apache.ibatis.session.SqlSession;
 
 import common.db.MyAppSqlConfig;
 import kr.co.bit_cinema.repository.mapper.MemberMapper;
+import kr.co.bit_cinema.repository.vo.MemberVO;
 
 @WebServlet("/member/Join")
 public class JoinServlet extends HttpServlet{
@@ -28,9 +29,34 @@ public class JoinServlet extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		MemberVO member = null;
 		try{
-//			response.sendRedirect(request.getContextPath() + "/main/Main");
+			String id = request.getParameter("id");
+			String pass = request.getParameter("pass");
+			String name = request.getParameter("name");
+			String nickname = request.getParameter("nickname");
+			String emailId = request.getParameter("emailId");
+			String emailAddr = request.getParameter("emailAddr");
+			String phone = request.getParameter("phone");
+			String agreeEmail = request.getParameter("agreeEmail");
+			String agreePhone = request.getParameter("agreePhone");
 		
+			member = new MemberVO();
+			member.setMemberId(id);
+			member.setPass(pass);
+			member.setName(name);
+			member.setNickname(nickname);
+			member.setPhone(phone);
+			member.setEmailId(emailId);
+			member.setEmailAddr(Integer.parseInt(emailAddr));
+			//member.setAgreeEmail(Integer.parseInt(agreeEmail));
+			member.setAgreeEmail(1);
+			//member.setAgreePhone(Integer.parseInt(agreePhone));
+			member.setAgreePhone(1);
+			
+			mapper.joinMember(member);
+			session.commit();
+			
 			RequestDispatcher rd = request.getRequestDispatcher("/view/member/join.jsp");
 			rd.forward(request, response);
 		
--- bit-cinema/src/kr/co/bit_cinema/repository/servlet/member/ModifyInfoFormServlet.java
+++ bit-cinema/src/kr/co/bit_cinema/repository/servlet/member/ModifyInfoFormServlet.java
@@ -8,12 +8,41 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+
+import common.db.MyAppSqlConfig;
+import kr.co.bit_cinema.repository.mapper.MemberMapper;
+import kr.co.bit_cinema.repository.vo.MemberVO;
 
 @WebServlet("/member/ModifyInfoForm")
 public class ModifyInfoFormServlet extends HttpServlet {
 
+	private SqlSession sqlSession = null;
+	private MemberMapper mapper = null;
+	
+	public ModifyInfoFormServlet() {
+		sqlSession = MyAppSqlConfig.getSqlSessionInstance();
+		mapper = sqlSession.getMapper(MemberMapper.class);
+	}
+	
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		MemberVO member = null;
+		try {
+			
+			member = mapper.infoMember(user.getMemberId());
+			sqlSession.commit();
+		
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		request.setAttribute("member", member);
 		
 		RequestDispatcher rd = request.getRequestDispatcher("/view/member/modifyInfoForm.jsp");
 		rd.forward(request, response);
